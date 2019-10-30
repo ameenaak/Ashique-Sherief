c123e368a401d37e18c0167e4facafbe02ebca45
Evin J Manuel <evinjrocks169@gmail.com>
2019-10-30 13:50:41 +0530

    Changes
    Changed the program completely

---
+++ beginning.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+{
+  int i;
+  for(i=0;i<10;++i)
+  {
+    printf("This is just printing number %d",i);
+  }
+  
+  printf("This is just printing numbers and nothing else feel free to modify as you please");
+}
